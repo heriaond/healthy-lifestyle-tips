4c235cdb4f40b3e6631efe41cafeaa3506e8a6a8
petr.herian1 <petr.herian1@newtonmedia.cz>
2025-11-18 16:27:33 +0100

    rf of types

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+// Re-export all category-related types and constants
+export {
+  CATEGORIES,
+  type Category,
+  categoryArray,
+  categoryIcons,
+  categoryDescriptions,
+} from "./categories";
+
+// Re-export all tip-related types
+export { type Tip } from "./tip";
